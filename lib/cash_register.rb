efd127cb4e955e22942e7a807f6a607904ca784d
Simeon <olabs06@yahoo.com>
2022-08-27 12:26:35 +0100

    creating register lab

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,33 @@
+require "pry"
 
+class CashRegister
+    attr_accessor :discount, :total, :item_history, :current_transaction
+    def initialize discount = nil
+        @total = 0
+        @discount = discount
+        @item_history = []
+    end
+
+    def add_item title, price, quantity=1
+        self.current_transaction = price * quantity
+        self.total += self.current_transaction
+        quantity.times {self.item_history << title}
+    end
+    def apply_discount
+        if self.discount
+            self.total -= self.total * self.discount/100
+            "After the discount, the total comes to $#{self.total}."
+            
+        else
+            "There is no discount to apply."
+        end
+    end
+
+    def items
+        self.item_history
+    end
+
+    def void_last_transaction
+        self.total -= self.current_transaction
+    end
+end
